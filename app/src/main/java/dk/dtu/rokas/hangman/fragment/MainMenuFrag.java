c909923dad6a2622deacaf89eba626583776619d
Armandas <armandas.rokas@gmail.com>
2019-09-29 15:47:58 +0200

    main menu fragment added

---
+++ app/src/main/java/dk/dtu/rokas/hangman/fragment/MainMenuFrag.java
@@ -0,0 +1,24 @@
+package dk.dtu.rokas.hangman.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import dk.dtu.rokas.hangman.R;
+
+public class MainMenuFrag extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View root = inflater.inflate(R.layout.main_menu, container, false);
+
+        return root;
+    }
+}
