8f738bc56421aa21843a1ea0e3cae8d3c1117e80
Armandas <armandas.rokas@gmail.com>
2019-10-12 10:27:55 +0200

    added username function in new game

--- app/src/main/java/dk/dtu/rokas/hangman/view/GameFrag.java
+++ app/src/main/java/dk/dtu/rokas/hangman/view/GameFrag.java
@@ -11,11 +11,11 @@ import androidx.fragment.app.Fragment;
 
 import dk.dtu.rokas.hangman.R;
 
-public class HighscoreFrag extends Fragment {
+public class GameFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.highscore, container, false);
+        return inflater.inflate(R.layout.game, container, false);
     }
 }
--- app/src/main/java/dk/dtu/rokas/hangman/view/NewGameFrag.java
+++ app/src/main/java/dk/dtu/rokas/hangman/view/NewGameFrag.java
@@ -4,19 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import dk.dtu.rokas.hangman.R;
+import dk.dtu.rokas.hangman.business.GameLogic;
+
 
 public class NewGameFrag extends Fragment {
+    private GameLogic gl = GameLogic.getInstance();
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.new_game, container, false);
+        final Button confirmBtn = v.findViewById(R.id.confirmUsernameBtn);
+        final EditText enterUsernameET = v.findViewById(R.id.usernameET);
 
-        return inflater.inflate(R.layout.game, container, false);
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = enterUsernameET.getText().toString();
+                gl.setCurrentUsername(username);
+            }
+        });
+
+        // Clears the initial text in the input field
+
+        enterUsernameET.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // FIXME requires two clicks in order to clear the text
+                enterUsernameET.getText().clear();
+            }
+        });
+
+        return v ;
     }
 
 }
