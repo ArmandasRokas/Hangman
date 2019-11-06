9f3c3fe9d4536a9eab42ea8d0c5813da1fe054ee
Armandas <armandas.rokas@gmail.com>
2019-11-06 18:01:53 +0100

    implemented functioning button for A letter

--- app/src/main/java/dk/dtu/rokas/hangman/view/GameFrag.java
+++ app/src/main/java/dk/dtu/rokas/hangman/view/GameFrag.java
@@ -1,6 +1,8 @@
 package dk.dtu.rokas.hangman.view;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -20,6 +23,8 @@ import dk.dtu.rokas.hangman.business.GameLogic;
 
 public class GameFrag extends Fragment {
     private GameLogic gl = GameLogic.getInstance();
+    private Button  a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, ae, oe, aa;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,10 +32,12 @@ public class GameFrag extends Fragment {
         TextView usernameTv = v.findViewById(R.id.usernameStatusTV);
         usernameTv.setText(String.format("%s",gl.getCurrentUsername() ));
         updateImage(v); //FIXME does not work when screen rotates
-        final TextView currLetters = v.findViewById(R.id.currLetters);
-        currLetters.setText(gl.getSynligtOrd());
-        final EditText guess = v.findViewById(R.id.guessET);
-        guess.setOnKeyListener(new View.OnKeyListener() {
+        initializeBtns(v);
+        updateButtons(v);
+        //final TextView currLetters = v.findViewById(R.id.currLetters);
+        //currLetters.setText(gl.getSynligtOrd());
+        //final EditText guess = v.findViewById(R.id.guessET);
+        /*guess.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View view, int keyCode, KeyEvent event) {
                 if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode==KeyEvent.KEYCODE_ENTER){
@@ -48,11 +55,45 @@ public class GameFrag extends Fragment {
             public void onClick(View view) {
                 confirmLetter(guess, currLetters, v);
             }
-        });
+        });*/
 
         return v;
     }
 
+    private void initializeBtns(View v) {
+        a = v.findViewById(R.id.aBtn);
+        a.setOnClickListener(v1 -> {
+            confirmLetter("a", v);
+        });
+    }
+
+    private void confirmLetter(String letter, View v){
+        gl.gætBogstav(letter);
+        if(gl.erSpilletTabt()){
+            Navigation.findNavController(v).navigate(R.id.action_gameFrag_to_loserFrag2);
+        }
+        if(gl.erSpilletVundet()){
+            Navigation.findNavController(v).navigate(R.id.action_gameFrag_to_winnerFrag);
+        }
+        showToaster();
+        updateImage(v);
+        updateButtons(v);
+    }
+
+    private void showToaster() {
+        CharSequence text;
+        int duration = Toast.LENGTH_SHORT;
+        if(gl.erSidsteBogstavKorrekt()){
+            text = "Good job!";
+        } else{
+            text = "Watch out!";
+        }
+        Toast toast = Toast.makeText(this.getContext(), text, duration);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
+/*
     private void confirmLetter(EditText guess, TextView currLetters, View v) {
         gl.gætBogstav(guess.getText().toString());
         guess.getText().clear();
@@ -60,13 +101,20 @@ public class GameFrag extends Fragment {
         updateImage(v);
         TextView usedLettersTv = v.findViewById(R.id.usedLattersTv);
         usedLettersTv.setText(String.format("Guessed letters: %s",gl.getBrugteBogstaver().toString() ));
-        if(gl.erSpilletTabt()){
+
+    }*/
+    /*
+        confirmLetter
+    - gl.gætBostav
+    - updateImage(v)
+    - update buttons
+            if(gl.erSpilletTabt()){
             Navigation.findNavController(v).navigate(R.id.action_gameFrag_to_loserFrag2);
         }
         if(gl.erSpilletVundet()){
             Navigation.findNavController(v).navigate(R.id.action_gameFrag_to_winnerFrag);
         }
-    }
+     */
 
     private void updateImage(View v) {
         ImageView imageView = v.findViewById(R.id.hangman);
@@ -91,4 +139,16 @@ public class GameFrag extends Fragment {
                 break;
         }
     }
+
+    private void updateButtons(View v){
+
+        if(gl.getBrugteBogstaver().contains("a")){
+            a.setBackgroundColor(getResources().getColor(R.color.colorGray));
+            a.setEnabled(false);
+        }
+        //char aChar = a.getText().charAt(0);
+        //
+
+    }
+
 }
